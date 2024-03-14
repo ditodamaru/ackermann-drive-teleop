361eee3763de7c54b9404cc5520aedc0ae2bbe17
ditodamaru <58300987+ditodamaru@users.noreply.github.com>
2024-03-15 03:49:45 +0900

    added safety features

--- scripts/joyop.py
+++ scripts/joyop.py
@@ -15,14 +15,19 @@ __maintainer__ = 'George Kouros'
 __email__ = 'gkourosg@yahoo.gr'
 
 import rospy
-from ackermann_msgs.msg import AckermannDriveStamped #AckermannDrive
+from ackermann_msgs.msg import AckermannDriveStamped
 from sensor_msgs.msg import Joy
+from std_msgs.msg import Int32
 import sys
 
 class AckermannDriveJoyop:
 
     def __init__(self, args):
-        if len(args)==1 or len(args)==2:
+        # Initialize ROS node
+        rospy.init_node('ackermann_drive_joyop_node')
+
+        # Initialize parameters
+        if len(args) == 1 or len(args) == 2:
             self.max_speed = float(args[0])
             self.max_steering_angle = float(args[len(args)-1])
             cmd_topic = 'ackermann_cmd'
@@ -31,29 +36,63 @@ class AckermannDriveJoyop:
             self.max_steering_angle = float(args[1])
             cmd_topic = '/' + args[2]
         else:
-            self.max_speed = 0.58  #0.2
-            self.max_steering_angle = 0.3 #0.44 #0.7
+            self.max_speed = 0.58
+            self.max_steering_angle = 0.39
             cmd_topic = 'ackermann_cmd'
 
+        # Initialize variables
         self.speed = 0
         self.steering_angle = 0
+        self.control_state = 0
+        self.connection_state = 0
         self.joy_sub = rospy.Subscriber('/joy', Joy, self.joy_callback)
-        self.drive_pub = rospy.Publisher(cmd_topic, AckermannDriveStamped,
-                                         queue_size=1) #AckermannDriveStamped
+        self.control_state_sub = rospy.Subscriber('/joy_control_state', Int32, self.control_state_callback)
+        self.connection_state_sub = rospy.Subscriber('/joy_connection_error', Int32, self.connection_state_callback)
+        self.drive_pub = rospy.Publisher(cmd_topic, AckermannDriveStamped, queue_size=1)
         rospy.Timer(rospy.Duration(1.0/5.0), self.pub_callback, oneshot=False)
         rospy.loginfo('ackermann_drive_joyop_node initialized')
 
     def joy_callback(self, joy_msg):
-        self.speed = joy_msg.axes[2] * self.max_speed#;
-        self.steering_angle = joy_msg.axes[3] * self.max_steering_angle#;
+        self.speed = joy_msg.axes[2] * self.max_speed
+        self.steering_angle = joy_msg.axes[3] * self.max_steering_angle
 
+    def control_state_callback(self, control_state_msg):
+        self.control_state = control_state_msg.data
+
+    def connection_state_callback(self, connection_state_msg):
+        self.connection_state = connection_state_msg.data
+        if self.connection_state == 0:
+            # Joystick connection error detected, stop the robot
+            rospy.logwarn("Joystick connection error detected. Stopping the robot.")
+            self.speed = 0
+            self.steering_angle = 0
+
+            ackermann_cmd_msg = AckermannDriveStamped()
+            ackermann_cmd_msg.drive.speed = self.speed
+            ackermann_cmd_msg.drive.steering_angle = self.steering_angle
+            self.drive_pub.publish(ackermann_cmd_msg)
+
+        else:
+            rospy.loginfo("Joystick connection established")
 
     def pub_callback(self, event):
-        ackermann_cmd_msg = AckermannDriveStamped() #AckermanDrive
-        ackermann_cmd_msg.drive.speed = self.speed
-        ackermann_cmd_msg.drive.steering_angle = self.steering_angle
-        self.drive_pub.publish(ackermann_cmd_msg)
-        self.print_state()
+        if self.control_state == 3:
+            ackermann_cmd_msg = AckermannDriveStamped()
+            ackermann_cmd_msg.drive.speed = self.speed
+            ackermann_cmd_msg.drive.steering_angle = self.steering_angle
+            self.drive_pub.publish(ackermann_cmd_msg)
+            self.print_state()
+        else:
+            # Reset speed and steering angle to zero
+            self.speed = 0
+            self.steering_angle = 0
+
+            # Publish zero values to stop the robot
+            ackermann_cmd_msg = AckermannDriveStamped()
+            ackermann_cmd_msg.drive.speed = self.speed
+            ackermann_cmd_msg.drive.steering_angle = self.steering_angle
+            self.drive_pub.publish(ackermann_cmd_msg)
+            #rospy.loginfo("Control state released. Stopping the robot.")
 
     def print_state(self):
         sys.stderr.write('\x1b[2J\x1b[H')
@@ -71,6 +110,5 @@ class AckermannDriveJoyop:
         sys.exit()
 
 if __name__ == '__main__':
-    rospy.init_node('ackermann_drive_joyop_node')
     joyop = AckermannDriveJoyop(sys.argv[1:len(sys.argv)])
     rospy.spin()
